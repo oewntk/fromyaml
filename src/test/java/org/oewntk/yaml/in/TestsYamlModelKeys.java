04c88a3acde98996f0b560efe3219bd8e38c020a
1313ou <1313ou@gmail.com>
2021-12-26 12:04:22 +0100

    tests revised

--- src/test/java/org/oewntk/yaml/in/TestsYamlModelKeys.java
+++ src/test/java/org/oewntk/yaml/in/TestsYamlModelKeys.java
@@ -8,6 +8,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.oewntk.model.LibTestModelKeys;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestsYamlModelKeys
 {
 	@BeforeClass
@@ -17,74 +19,155 @@ public class TestsYamlModelKeys
 	}
 
 	@Test
+	public void testMobile()
+	{
+		int[] r = LibTestModelKeys.testMobile(TestsYamlCommon.model, TestsYamlCommon.ps);
+		assertEquals(1, r[0]);
+		assertEquals(1, r[1]);
+		assertEquals(1, r[2]);
+		assertEquals(2, r[3]);
+		assertEquals(2, r[4]);
+		assertEquals(5, r.length);
+	}
+
+	@Test
+	public void testMobileNoPronunciation()
+	{
+		int[] r = LibTestModelKeys.testMobileNoPronunciation(TestsYamlCommon.model, TestsYamlCommon.ps);
+		assertEquals(1, r[0]);
+		assertEquals(1, r[1]);
+		assertEquals(2, r[2]);
+		assertEquals(2, r[3]);
+		assertEquals(4, r.length);
+	}
+
+	@Test
 	public void testEarthMulti()
 	{
-		LibTestModelKeys.testEarthMulti(TestsYamlCommon.model, TestsYamlCommon.ps);
+		int[] r = LibTestModelKeys.testEarthMulti(TestsYamlCommon.model, TestsYamlCommon.ps);
+		assertEquals(1, r[0]);
+		assertEquals(1, r[1]);
+		assertEquals(2, r[2]);
+		assertEquals(2, r[3]);
+		assertEquals(4, r.length);
 	}
 
 	@Test
 	public void testEarthMono()
 	{
-		LibTestModelKeys.testEarthMono(TestsYamlCommon.model, TestsYamlCommon.ps);
+		int[] r = LibTestModelKeys.testEarthMono(TestsYamlCommon.model, TestsYamlCommon.ps);
+		assertEquals(1, r[0]);
+		assertEquals(1, r[1]);
+		assertEquals(1, r[2]);
+		assertEquals(1, r[3]);
+		assertEquals(4, r.length);
 	}
 
 	@Test
 	public void testBaroqueMulti()
 	{
-		LibTestModelKeys.testBaroqueMulti(TestsYamlCommon.model, TestsYamlCommon.ps);
+		int[] r = LibTestModelKeys.testBaroqueMulti(TestsYamlCommon.model, TestsYamlCommon.ps);
+		assertEquals(1, r[0]);
+		assertEquals(1, r[1]);
+		assertEquals(2, r[2]);
+		assertEquals(2, r[3]);
+		assertEquals(1, r[4]);
+		assertEquals(2, r[5]);
+		assertEquals(3, r[6]);
+		assertEquals(3, r[7]);
+		assertEquals(0, r[8]);
+		assertEquals(1, r[9]);
+		assertEquals(1, r[10]);
+		assertEquals(1, r[11]);
+		assertEquals(0, r[12]);
+		assertEquals(0, r[13]);
+		assertEquals(0, r[14]);
+		assertEquals(0, r[14]);
+		assertEquals(16, r.length);
 	}
 
 	@Test
 	public void testBaroqueMono()
 	{
-		LibTestModelKeys.testBaroqueMono(TestsYamlCommon.model, TestsYamlCommon.ps);
+		int[] r = LibTestModelKeys.testBaroqueMono(TestsYamlCommon.model, TestsYamlCommon.ps);
+		assertEquals(1, r[0]);
+		assertEquals(1, r[1]);
+		assertEquals(1, r[2]);
+		assertEquals(1, r[3]);
+		assertEquals(1, r[4]);
+		assertEquals(1, r[5]);
+		assertEquals(1, r[6]);
+		assertEquals(1, r[7]);
+		assertEquals(0, r[8]);
+		assertEquals(1, r[9]);
+		assertEquals(1, r[10]);
+		assertEquals(1, r[11]);
+		assertEquals(0, r[12]);
+		assertEquals(0, r[13]);
+		assertEquals(0, r[14]);
+		assertEquals(0, r[14]);
+		assertEquals(16, r.length);
 	}
 
 	@Test
-	public void testMobile()
+	public void testCriticalMulti()
 	{
-		LibTestModelKeys.testMobile(TestsYamlCommon.model, TestsYamlCommon.ps);
+		int[] r = LibTestModelKeys.testCriticalMulti(TestsYamlCommon.model, TestsYamlCommon.ps);
+		assertEquals(1, r[0]);
+		assertEquals(2, r[1]);
+		assertEquals(1, r[2]);
+		assertEquals(0, r[3]);
+		assertEquals(4, r.length);
+	}
+
+	@Test
+	public void testCriticalMono()
+	{
+		int[] r = LibTestModelKeys.testCriticalMono(TestsYamlCommon.model, TestsYamlCommon.ps);
+		assertEquals(1, r[0]);
+		assertEquals(1, r[1]);
+		assertEquals(1, r[2]);
+		assertEquals(0, r[3]);
+		assertEquals(4, r.length);
 	}
 
 	@Test
 	public void testBassDeep()
 	{
-		LibTestModelKeys.testBassDeep(TestsYamlCommon.model, TestsYamlCommon.ps);
+		int[] r = LibTestModelKeys.testBassDeep(TestsYamlCommon.model, TestsYamlCommon.ps);
+		assertEquals(1, r[0]);
+		assertEquals(1, r[1]);
+		assertEquals(2, r[2]);
+		assertEquals(3, r.length);
 	}
 
 	@Test
 	public void testBassShallow()
 	{
-		LibTestModelKeys.testBassShallow(TestsYamlCommon.model, TestsYamlCommon.ps);
+		int[] r = LibTestModelKeys.testBassShallow(TestsYamlCommon.model, TestsYamlCommon.ps);
+		assertEquals(1, r[0]);
+		assertEquals(1, r[1]);
+		assertEquals(2, r[2]);
+		assertEquals(3, r.length);
 	}
 
 	@Test
 	public void testRowDeep()
 	{
-		LibTestModelKeys.testRowDeep(TestsYamlCommon.model, TestsYamlCommon.ps);
+		int[] r = LibTestModelKeys.testRowDeep(TestsYamlCommon.model, TestsYamlCommon.ps);
+		assertEquals(1, r[0]);
+		assertEquals(1, r[1]);
+		assertEquals(2, r[2]);
+		assertEquals(3, r.length);
 	}
 
 	@Test
 	public void testRowShallow()
 	{
-		LibTestModelKeys.testRowShallow(TestsYamlCommon.model, TestsYamlCommon.ps);
-	}
-
-	@Test
-	public void testCriticalDeep()
-	{
-		LibTestModelKeys.testCriticalDeep(TestsYamlCommon.model, TestsYamlCommon.ps);
-	}
-
-	@Test
-	public void testCriticalPos()
-	{
-		LibTestModelKeys.testCriticalPos(TestsYamlCommon.model, TestsYamlCommon.ps);
-	}
-
-	@Test
-	public void testCriticalPWN()
-	{
-		LibTestModelKeys.testCriticalPWN(TestsYamlCommon.model, TestsYamlCommon.ps);
+		int[] r = LibTestModelKeys.testRowShallow(TestsYamlCommon.model, TestsYamlCommon.ps);
+		assertEquals(1, r[0]);
+		assertEquals(1, r[1]);
+		assertEquals(2, r[2]);
+		assertEquals(3, r.length);
 	}
 }
