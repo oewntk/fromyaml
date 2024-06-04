bb7a5bd1ecc9b3e56ffcf63a738b147f7a3c35df
1313ou <1313ou@gmail.com>
2024-06-04 08:45:57 +0200

    test on yaml parser handling of quoting

---
+++ src/test/java/org/oewntk/yaml/in/TestsYamlQuotes.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2021-2024. Bernard Bou.
+ */
+package org.oewntk.yaml.`in`
+
+import org.junit.BeforeClass
+import org.junit.Ignore
+import org.junit.Test
+import org.oewntk.yaml.`in`.LibTestsYamlCommon.model
+import org.oewntk.yaml.`in`.LibTestsYamlCommon.ps
+
+@Ignore
+class TestsYamlQuotes {
+
+    @Test
+    fun testQuote() {
+        val synset = model!!.synsetsById?.get("06853940-n")!!
+        synset.examples?.forEach {
+            ps.println(it)
+            assert(it.startsWith("`"))
+        }
+    }
+
+    companion object {
+
+        @JvmStatic
+        @BeforeClass
+        fun init() {
+            LibTestsYamlCommon.init()
+            checkNotNull(model)
+        }
+    }
+}
