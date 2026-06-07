01bf3143a279f662b10acd40a640955bc4a4a9ce
1313ou <1313ou@gmail.com>
2026-06-07 12:02:29 +0200

    model comparison

--- src/main/java/org/oewntk/yaml/in/Factory.kt
+++ src/main/java/org/oewntk/yaml/in/Factory.kt
@@ -110,7 +110,7 @@ class Factory(
         @JvmStatic
         fun main(args: Array<String>) {
             val model = makeModel(args)
-            Tracing.psInfo.printf("[Model] %s%n%s%n%s%n", model!!.sources.contentToString(), model.info(), ModelInfo.counts(model))
+            Tracing.psInfo.printf("[Model] %s%n%s%n%s%n", model!!, model.info(), ModelInfo.counts(model))
         }
     }
 }
