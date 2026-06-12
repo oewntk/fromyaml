54c7051bc7c7ec43f75ebb0f92e6f5d014903120
1313ou <1313ou@gmail.com>
2026-06-12 16:58:52 +0200

    align option syntax

--- src/main/java/org/oewntk/yaml/in/FactoryPlus.kt
+++ src/main/java/org/oewntk/yaml/in/FactoryPlus.kt
@@ -48,7 +48,7 @@ class FactoryPlus(
         fun makeModel(args: Array<String>): Model? {
             var iArg = 0
             var verbose = false
-            if (args[iArg] == "-verbose") {
+            if (args[iArg] == "--verbose") {
                 verbose = true
                 iArg++
             }
