e6a8bdb6190d9319b7aea733fb02b08f9ad5b492
1313ou <1313ou@gmail.com>
2026-06-14 11:40:02 +0200

    extend tests

--- src/test/java/org/oewntk/yaml/in/LibTestsYamlCommon.kt
+++ src/test/java/org/oewntk/yaml/in/LibTestsYamlCommon.kt
@@ -4,13 +4,18 @@
 package org.oewntk.yaml.`in`
 
 import org.junit.Assert
-import org.oewntk.model.CoreModel
+import org.oewntk.model.Model
+import org.oewntk.model.ModelInfo
 import java.io.File
 import java.io.PrintStream
+import kotlin.test.assertEquals
 
 object LibTestsYamlCommon {
 
     private val source: String? = System.getProperty("SOURCE")
+    private val sourceB: String? = System.getProperty("SOURCEB")
+    private val source2: String? = System.getProperty("SOURCE2")
+    private val sourceB2: String? = System.getProperty("SOURCEB2")
 
     val silent = if (System.getProperties().containsKey("VERBOSE")) false
     else if (System.getProperties().containsKey("SILENT")) true
@@ -18,18 +23,40 @@ object LibTestsYamlCommon {
 
     val ps: PrintStream = if (!silent) Tracing.psInfo else Tracing.psNull
 
-    val model: CoreModel by lazy {
-        if (source == null) {
-            Tracing.psErr.println("Define yaml source dir with -DSOURCE=path")
-            throw AssertionError("SOURCE not defined")
-        }
+    fun checkOrig() {
+        val orig: String = System.getProperty("INFO")!!
+        val origInfo = File(orig).readText()
+        val info = model.info()
+        val counts = ModelInfo.counts(model)
+        val modelInfo = "$info\n$counts"
+        ps.println(modelInfo)
+        assertEquals(origInfo, modelInfo)
+    }
+
+    private fun getModel(source: String, source2: String?): Model {
         val inDir = File(source)
+        val inDir2 = if (source2 == null) null else File(source)
         Tracing.psInfo.printf("source=%s%n", inDir.absolutePath)
         if (!inDir.exists()) {
             Tracing.psErr.println("Define YAML source dir that exists")
             Assert.fail()
         }
+        return Factory(inDir, inDir2, throws = true, inverses = false, verbose = !silent).get()!!
+    }
 
-        CoreFactory(inDir, throws = true, inverses = false, verbose = !silent).get()!!
+    val model: Model by lazy {
+        if (source == null) {
+            Tracing.psErr.println("Define serialized source file dir with -DSOURCE=path")
+            throw AssertionError("SOURCE not defined")
+        }
+        getModel(source, source2)
+    }
+
+    val modelB: Model by lazy {
+        if (sourceB == null) {
+            Tracing.psErr.println("Define serialized source file dir with -DSOURCEB=path")
+            throw AssertionError("SOURCEB not defined")
+        }
+        getModel(sourceB, sourceB2)
     }
 }
